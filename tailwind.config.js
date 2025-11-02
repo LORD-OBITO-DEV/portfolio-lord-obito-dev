61fc82910e48ed1b64f3da77acfb296b80171380
オビト・デヴ卿🐉🫟 <nguessanjeanstephane87@gmail.com>
2025-11-02 23:34:54 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#0b74ff',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  darkMode: "class", // important
+};
